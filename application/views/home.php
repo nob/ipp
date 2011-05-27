3aff9acb1b874d2bd36591f88da8c286e3f77a43
nob <noboruthedragon@gmail.com>
2011-05-27 00:43:17 +0000

    Added Google Analytics tracking code.
    git-svn-id: https://koholasys.springloops.com/source/ippuku/trunk@75 f7f11f79-1d17-4e1d-a425-2e0c31adf06e

--- application/views/home.php
+++ application/views/home.php
@@ -17,6 +17,19 @@
         <!--<script type="text/javascript" src="<?php echo site_url('js/overlay.apple.js');?>"></script>-->
         <script type="text/javascript" src="<?php echo site_url('js/toolbox.flashembed.js');?>"></script>
 		<script type="text/javascript" src="<?php echo site_url('js/ippuku.js');?>"></script>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-23614931-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 	</head>
 <body>		
     <noscript>    
