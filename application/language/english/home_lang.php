611d9035327cc31afdd802177a1767c82ecf3cdf
nob <noboruthedragon@gmail.com>
2011-06-02 13:47:04 +0000

    Modified meta description again.
    git-svn-id: https://koholasys.springloops.com/source/ippuku/trunk@88 f7f11f79-1d17-4e1d-a425-2e0c31adf06e

--- application/language/english/home_lang.php
+++ application/language/english/home_lang.php
@@ -7,7 +7,7 @@
 */
 
 $lang['html_title'] = 'Ippuku | Yakitori and Shochu dining bar in Berkeley, CA.'; 
-$lang['meta_description'] = 'Yakitori and Shochu. Japanese Izakaya style dining bar. 2130 Center Street, Berkeley, CA. Reservation: (510) 665-1969 ';
+$lang['meta_description'] = 'Yakitori and Shochu. Japanese Izakaya style dining bar. Address: 2130 Center Street, Berkeley, CA. Reservation: (510) 665-1969 ';
 //$lang['about']  = 'About';
 $lang['dirc']   = 'Directions';
 $lang['hour']   = 'Hours';
