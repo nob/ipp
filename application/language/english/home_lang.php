362e114e3d6ce5a3be1d80514ad2a98a92cd7f22
Nob Y <noboruthedragon@gmail.com>
2012-08-03 01:45:07 +0900

    Modified title.

--- application/language/english/home_lang.php
+++ application/language/english/home_lang.php
@@ -6,7 +6,7 @@
 |
 */
 
-$lang['html_title'] = 'Ippuku | Yakitori and Shochu dining bar in Berkeley, CA.'; 
+$lang['html_title'] = 'Ippuku | Soba lunch, Izakaya dinner in Berkeley, CA.'; 
 $lang['meta_description'] = 'Yakitori and Shochu. Japanese Izakaya style dining bar. Address: 2130 Center Street, Berkeley, CA. Reservation: (510) 665-1969 ';
 $lang['new']   = 'new!';
 $lang['tori_hour']   = 'Yakitori hours';
