6894616fc8d2ee3a2752d27f14c5a7693cea5453
Nob Y <noboruthedragon@gmail.com>
2013-01-01 01:13:53 +0900

    Correct direction instructions.

--- application/language/english/directions_lang.php
+++ application/language/english/directions_lang.php
@@ -7,11 +7,11 @@
 */
 
 $lang['desc1']	= 
-"We are located at 2130 Center Street (between Shattuck and University) in Berkeley across from the downtown BART station.";
+"We are located at 2130 Center Street (between Shattuck and Oxford) in Berkeley across from the downtown BART station.";
 $lang['header1'] = "Driving from I-80"; 
 $lang['desc2']	= 
 "Take University exit to University Ave east bound.
 Turn right on Shattuck and then left on Center.
 Ippuku will be located on the right side of Center Street.
 
-Parking is available on the street or in the adjacent BofA lot .";
+Paid parking is available on the street or in the adjacent B of A lot .";
--- application/language/japanese/directions_lang.php
+++ application/language/japanese/directions_lang.php
@@ -7,7 +7,7 @@
 */
 
 $lang['desc1']	= 
-"住所: 2130 Center Street Berkeley, CA 94704 
+"住所: 2130 Center Street (between Shattuck Ave and Oxford St) Berkeley, CA 94704 
 Shattuck Aveを挟んだDowntown Berkeley駅(BART)の向かいにあります。";
 $lang['header1'] = "I-80 からお車でお越しの方";
 $lang['desc2']	= 
@@ -15,4 +15,4 @@ $lang['desc2']	=
 Shattuck Ave で右折し、2つ目の角を左に曲がってCenter Streetに入ります.
 Center Streetの右手にあるお店が一服です。
 
-駐車には路上、もしくは隣接の駐車場をお使い頂けます。";
+駐車には路上、もしくはBank of America隣接の有料駐車場をお使い頂けます。";
