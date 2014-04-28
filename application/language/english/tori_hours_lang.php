5e905f19ec825ddd956ce2394c79f432025b49d6
N. Yanbe <yanbe.n@jointanet.com>
2014-04-28 23:20:17 +0900

    Put 'Soba' section back.

--- application/language/english/tori_hours_lang.php
+++ application/language/english/tori_hours_lang.php
@@ -1,13 +1,13 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| Texts displayed in view files. 
+| Texts displayed in view files.
 |--------------------------------------------------------------------------
 |
 */
 
-$lang['title']	= "Yakitori Dinner- Open Hours"; 
-$lang['desc']	=  
-"Monday - Thursday, Sunday: 5:00pm - 10:00pm
+$lang['title']	= "Yakitori Dinner- Open Hours";
+$lang['desc']	=
+"Wednesday, Thursday, Sunday: 5:00pm - 10:00pm
 Friday, Saturday: 5:00pm - 11:00pm";
 
